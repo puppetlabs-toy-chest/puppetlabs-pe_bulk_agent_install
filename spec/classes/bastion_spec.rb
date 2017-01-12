d53ffe05878dcfb4029b7af4f27ab2660a3f0c44
Nate McCurdy <nate.mccurdy@puppet.com>
2017-01-12 12:51:39 -0800

    Add basic spec tests
    * Test the bastion class and the WinRM script creation
    * Test the bulk application existance

---
+++ spec/classes/bastion_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "pe_bulk_agent_install::windows::bastion" do
+
+  before :each do
+    Puppet.settings[:server] = 'puppet.master.local'
+  end
+
+  context "on Windows with custom parameters" do
+
+    let(:facts) {{
+      :osfmaily => 'windows',
+      :kernel   => "windows"
+    }}
+
+    let(:params) {{
+      # Need to set the location to a POSIX path for the spec test to work:
+      # https://github.com/rodjek/rspec-puppet/issues/192
+      :scripts_install_location => '/tmp',
+      :master_port              => 9999,
+      :script_name              => 'meow.ps1'
+    }}
+
+    it { is_expected.to compile.with_all_deps }
+
+    it { should contain_file('Windows Puppet Agent Bulk Installer').with_ensure('file') }
+    it { should contain_file('Windows Puppet Agent Bulk Installer').with_path('/tmp/Invoke-PuppetAgentInstall.ps1') }
+    it { should contain_file('Windows Puppet Agent Bulk Installer').with_content(/puppet\.master\.local/) }
+    it { should contain_file('Windows Puppet Agent Bulk Installer').with_content(/9999/) }
+    it { should contain_file('Windows Puppet Agent Bulk Installer').with_content(/meow\.ps1/) }
+
+  end
+
+end
+
---
+++ spec/unit/puppet/application/bulk_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'puppet/application/bulk'
+
+describe Puppet::Application::Bulk do
+
+  it { should be_a_kind_of Puppet::Application::FaceBase }
+
+end
+
