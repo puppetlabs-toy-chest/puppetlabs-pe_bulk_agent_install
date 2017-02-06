cc8c59b6fab92333b55b8a55caf79f353bd11846
Nate McCurdy <nate.mccurdy@puppet.com>
2017-02-05 23:03:58 -0600

    Update spec tests for all supported OS's

--- spec/classes/bastion_spec.rb
+++ spec/classes/bastion_spec.rb
@@ -5,47 +5,45 @@ describe 'pe_bulk_agent_install::windows::bastion' do
     Puppet.settings[:server] = 'puppet.master.local'
   end
 
-  context 'on Windows with custom parameters' do
-    let(:facts) do
-      {
-        os: { family: 'windows' }
-      }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      case facts[:os][:family]
+      when 'windows'
+
+        let(:params) do
+          {
+            # Need to set the location to a POSIX path for the spec test to work:
+            # https://github.com/rodjek/rspec-puppet/issues/192
+            scripts_install_location: '/tmp',
+            master_port:              9999,
+            script_name:              'meow.ps1'
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to contain_file('Windows Puppet Agent Bulk Installer').with_ensure('file') }
+        it { is_expected.to contain_file('Windows Puppet Agent Bulk Installer').with_path('/tmp/Invoke-PuppetAgentInstall.ps1') }
+        it { is_expected.to contain_file('Windows Puppet Agent Bulk Installer').with_content(%r{puppet\.master\.local}) }
+        it { is_expected.to contain_file('Windows Puppet Agent Bulk Installer').with_content(%r{9999}) }
+        it { is_expected.to contain_file('Windows Puppet Agent Bulk Installer').with_content(%r{meow\.ps1}) }
+
+      else
+
+        let(:params) do
+          {
+            # Need to set the location to a POSIX path for the spec test to work:
+            # https://github.com/rodjek/rspec-puppet/issues/192
+            scripts_install_location: '/tmp'
+          }
+        end
+
+        it { is_expected.to raise_error(%r{class must only be applied to Windows agents}) }
+      end
     end
-
-    let(:params) do
-      {
-        # Need to set the location to a POSIX path for the spec test to work:
-        # https://github.com/rodjek/rspec-puppet/issues/192
-        scripts_install_location: '/tmp',
-        master_port:              9999,
-        script_name:              'meow.ps1'
-      }
-    end
-
-    it { is_expected.to compile.with_all_deps }
-
-    it { is_expected.to contain_file('Windows Puppet Agent Bulk Installer').with_ensure('file') }
-    it { is_expected.to contain_file('Windows Puppet Agent Bulk Installer').with_path('/tmp/Invoke-PuppetAgentInstall.ps1') }
-    it { is_expected.to contain_file('Windows Puppet Agent Bulk Installer').with_content(%r{puppet\.master\.local}) }
-    it { is_expected.to contain_file('Windows Puppet Agent Bulk Installer').with_content(%r{9999}) }
-    it { is_expected.to contain_file('Windows Puppet Agent Bulk Installer').with_content(%r{meow\.ps1}) }
-  end
-
-  context 'on a non-Windows server' do
-    let(:facts) do
-      {
-        os: { family: 'RedHat' }
-      }
-    end
-
-    let(:params) do
-      {
-        # Need to set the location to a POSIX path for the spec test to work:
-        # https://github.com/rodjek/rspec-puppet/issues/192
-        scripts_install_location: '/tmp'
-      }
-    end
-
-    it { is_expected.to raise_error(%r{class must only be applied to Windows agents}) }
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
+require 'rspec-puppet-facts'
+include RspecPuppetFacts
+
 if Puppet.version.to_f >= 4.5
   RSpec.configure do |c|
     c.before :each do
