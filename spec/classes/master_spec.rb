53cf41ab83d07f7eebd5236c574c9f3473ca48fb
Nate McCurdy <nate.mccurdy@puppet.com>
2017-04-24 08:50:40 -0700

    Fix the check for pre-existing install.ps1
    Prior to this, there was an extra `$` symbol in the if statement that
    checks for an existing install.ps1. While the code still compiled and
    worked, it's not proper style to have the extra `$`.
    
    Also added some spec tests to double-check that that if statement is
    working and hopefully catch issues and regessions like this in the
    future.

--- spec/classes/master_spec.rb
+++ spec/classes/master_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'pe_bulk_agent_install::windows::master' do
   let(:pre_condition) { "class pe_repo( $master = 'puppet' ) {}" }
 
-  context 'on a Puppet Enterprise master' do
+  context 'on a Puppet Enterprise master older than 2016.3' do
     let(:facts) do
       {
         pe_server_version: '2015.2.3',
@@ -19,6 +19,7 @@ describe 'pe_bulk_agent_install::windows::master' do
       it { is_expected.to contain_file('/opt/puppetlabs/server/data/packages/public/2015.2.3/install.ps1').with_content(%r{^\$port\s+=\s+'8140'$})  }
 
       it { is_expected.to contain_class('pe_bulk_agent_install::chloride') }
+      it { is_expected.to contain_class('pe_repo').that_comes_before('Class[pe_bulk_agent_install::windows::master]') }
     end
 
     context 'with install_chloride set to false' do
@@ -36,5 +37,21 @@ describe 'pe_bulk_agent_install::windows::master' do
 
   context 'on a non-Puppet Enterprise master' do
     it { is_expected.to raise_error(%r{should only be applied to a Puppet Master}) }
+  end
+
+  context 'on a Puppet Enterprise master that already has an install.ps1' do
+    let(:pre_condition) { "file { '/opt/puppetlabs/server/data/packages/public/2016.5.1/install.ps1': }" }
+
+    let(:facts) do
+      {
+        pe_server_version: '2016.5.1',
+        settings: { masterport: '8140' }
+      }
+    end
+
+    context 'with default parameters' do
+      it { is_expected.to compile }
+      it { is_expected.not_to contain_class('pe_repo') }
+    end
   end
 end
